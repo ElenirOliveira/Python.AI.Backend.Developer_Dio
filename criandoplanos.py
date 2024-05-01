848db4a50de73ab2f96935da284aa124517093c8
ELENIR ARAUJO OLIVEIRA <151475884+ElenirOliveira@users.noreply.github.com>
2024-04-30 21:01:28 -0300

    verificador.de.planos.py

--- criandoplanos.py
+++ criandoplanos.py
@@ -1,1 +1,13 @@
+def recomendar_plano(consumo_mensal: float) -> str:
+    if consumo_mensal <= 10:
+        return "Plano Essencial Fibra - 50Mbps"
+    elif consumo_mensal <= 20:
+        return "Plano Prata Fibra - 100Mbps"
+    else:
+        return "Plano Premium Fibra - 300Mbps"
+        
+# Solicita ao usuário que insira o consumo médio mensal de dados:
+consumo = float(input("Digite o consumo medio mensal de dados em GB: "))
 
+  # Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
+print("O plano que te indicamos é:", recomendar_plano(consumo))
